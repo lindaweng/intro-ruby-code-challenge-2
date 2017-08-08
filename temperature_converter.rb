5061f2c1c7372160bcac6da7f7e0501997cbc06a
Linda Weng <lindaweng4@gmail.com>
2017-08-08 15:44:25 +0000

    added new ruby file and completed the to-do

--- temperature_converter.rb
+++ temperature_converter.rb
@@ -1,1 +1,13 @@
+# Converts temperature from Fahrenheit to Celsius and back
 
+def fToC(temp)
+   return "The temperature in Celsius is " + ((temp - 32 ) * 5/9.0).to_s + " degrees."
+end
+
+puts fToC(60)
+
+def cToF(temp)
+   return "The temperature in Fahrenheit is " + ((temp * 9.0/5) + 32).to_s + " degrees."
+end
+
+puts cToF(25)
